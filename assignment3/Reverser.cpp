a4575587e2a8eccb656346f4beeb3a925630760d
James Illman <james.illman0@gmail.com>
2025-03-20 19:08:15 +1030

    i think

---
+++ assignment3/Reverser.cpp
@@ -0,0 +1,40 @@
+#include "Reverser.h"
+#include <iostream>
+
+using namespace std;
+
+Reverser::Reverser(/* args */)
+{
+}
+
+Reverser::~Reverser()
+{
+}
+
+int Reverser::reverseDigit(int value, int acc){
+    if (value == 0) {
+        return acc;
+    }
+    int lastDigit = value%10;
+    acc = acc * 10 + lastDigit;
+    return reverseDigit(value/10, acc);
+}
+
+int Reverser::reverseDigit(int value) {
+    return reverseDigit(value, 0);
+}
+
+string Reverser::reverseString(string characters, string acc){
+    if (characters.empty()) {
+        return acc;
+    }
+
+    char firstChar = characters[0];
+    acc = firstChar + acc;
+
+    return reverseString(characters.substr(1), acc);
+}
+
+string Reverser::reverseString(string characters) {
+    return reverseString(characters, "");
+}
--- assignment3/main.cpp
+++ assignment3/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
 #include "Truckloads.h"
+#include "Reverser.h"
 
 
 int main(void){
+    /*
     int numCrates = 14;
     int loadSize = 3;
 
@@ -11,4 +13,12 @@ int main(void){
 
     std::cout << t.numTrucks(numCrates, loadSize) << std::endl;
 
+    */
+
+    Reverser r;
+    int reversedNumber = r.reverseDigit(12345);
+    std::cout << reversedNumber << std::endl;
+
+    string reversedString = r.reverseString("James");
+    std::cout << reversedString << std::endl;
 }
