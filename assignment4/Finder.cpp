acd389dfcf54e8a508b56b0b329041010514a784
James Illman <james.illman0@gmail.com>
2025-04-05 10:52:29 +1030

    please

--- assignment4/Finder.cpp
+++ assignment4/Finder.cpp
@@ -5,17 +5,24 @@
     vector<int> Finder::findSubstrings(string s1, string s2) {
 
         vector<int> result;
+        string currentPrefix = "";
 
-        for(size_t i = 1; i <= s2.size(); i++) {
-            size_t found = s1.find(s2.substr(0, i));
-            if (found != string::npos) {
-                result.push_back(found);
-            } else {
-
-                result.push_back(-1);
-                
-                // If no prefix is found then all subsequent prefixs will also not be found
-                result.resize(s2.size(),-1);
+        for (size_t i = 0; i < s2.size(); ++i) {
+            currentPrefix += s2[i];
+            // Do a manual prefix search to avoid substr overhead
+            bool found = false;
+    
+            for (size_t j = 0; j + currentPrefix.size() <= s1.size(); ++j) {
+                if (s1.compare(j, currentPrefix.size(), currentPrefix) == 0) {
+                    result.push_back(j);
+                    found = true;
+                    break;
+                }
+            }
+    
+            if (!found) {
+                // fill rest with -1s
+                result.resize(s2.size(), -1);
                 break;
             }
         }
