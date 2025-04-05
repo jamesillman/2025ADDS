c0df787fc4bf5b6fcd640a4a94aba98190552605
James Illman <james.illman0@gmail.com>
2025-04-05 11:10:03 +1030

    d

--- assignment4/Finder.cpp
+++ assignment4/Finder.cpp
@@ -3,20 +3,24 @@ using namespace std;
 
 vector<int> Finder::findSubstrings(string s1, string s2) {
     vector<int> result;
-    size_t start_index = 0;  // Start index for find()
+    size_t start_index = 0;
 
     for (size_t i = 1; i <= s2.size(); ++i) {
-        size_t found = s1.find(s2.substr(0, i), start_index);  // Find prefix starting from start_index
-        
+        size_t found = s1.find(s2.substr(0, i), start_index);
+
         if (found != string::npos) {
             result.push_back(found);
-            start_index = found + 1;  // Update start_index to continue searching after the found prefix
+            start_index = found + 1;
         } else {
-            result.push_back(-1);  // If not found, return -1 for this prefix
-            result.resize(s2.size(), -1);  // Fill remaining with -1s
+            result.push_back(-1);
             break;
         }
     }
 
+    if (result.size() < s2.size()) {
+        result.resize(s2.size(), -1);
+    }
+
     return result;
 }
+
