2b71d716ccd7efaa171a558138fde56ab7c6063d
James Illman <james.illman0@gmail.com>
2025-05-21 11:30:07 +0930

    b

--- assignment8/klargest.cpp
+++ assignment8/klargest.cpp
@@ -1,7 +1,18 @@
 #include <vector>
 #include <queue>
 
+using namespace std;
 
 int kth_largest(vector<int> values, int k) {
     std::priority_queue<int> maxHeap;
+
+    for (int value : values) {
+    maxHeap.push(-value);
+    
+    if (maxHeap.size() > k) {
+        maxHeap.pop();
+        }  
+    }
+    
+    return maxHeap.top();
 }
